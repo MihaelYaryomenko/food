f3ae9a535d0f4c0677925f1da7a3d91d8210d1c7
Mihael Yaryomenko <mihaelyaryomenko2005@gmail.com>
2024-06-26 18:10:13 +0700

    Header finish

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import './App.scss'
 import RouteButton from './components/RouteButton'
+import { ReactComponent as SvgUser } from './images/user.svg'
+import { ReactComponent as SvgSearch } from './images/search.svg'
 
 function App() {
   return (
     <>
       <header id="main-header">
-        <div id="logo-container">
-          <p id="logo-text">SOON</p>
-          <img id="logo" src="logo.svg" alt="logo" />
+        <div id="left-header-container">
+          <div id="logo-container">
+            <p id="logo-text">SOON</p>
+            <img id="logo" src="logo.svg" alt="logo" />
+          </div>
+          <div id="route-container">
+            <RouteButton text={'Новосибирск'} />
+            <RouteButton text={'Меню'} />
+            <RouteButton text={'Ресторан'} />
+            <RouteButton text={'Акции'} />
+          </div>
         </div>
-        <div id="route-container">
-          <RouteButton text={'Новосибирск'} />
-          <RouteButton text={'Меню'} />
-          <RouteButton text={'Ресторан'} />
-          <RouteButton text={'Акции'} />
-        </div>
-        <div id="account-container">
-          <img src="user.svg" alt="" />
+        <div id="right-header-container">
+          <div id="search-container">
+            <input placeholder="поиск..." id="search-input" type="text" />
+            <SvgSearch id="svg-search" style={{ fill: 'black' }} />
+          </div>
+          <div id="account-container">
+            <SvgUser id="svg-user" style={{ fill: 'white' }} />
+            <p id="profile-text">Войти</p>
+          </div>
         </div>
       </header>
       <div id="main-container"></div>
